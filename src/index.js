2271c26e366c862c10ae1a29c1b1373e047b666c
Manoel Roberto <manoelrobertoms@gmail.com>
2022-06-15 22:30:03 -0300

    Created the user creation request.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,38 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Primeiro teste
 }
 
 app.post('/users', (request, response) => {
-  // Segundo teste
+  const { name, username} = request.body;
+  
+  const usealreadyexist = users.some(user => user.username === username);
+
+  if(usealreadyexist){
+    return response.status(400).send({error: "User already exists!"});
+  }
+
+  const user = {
+    id: new uuidv4(),
+    name: name,
+    username: username,
+    todo: []
+  }
+
+  users.push(user)
+
+  return response.status(201).send({ message: "User created successfully!"})
+
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
